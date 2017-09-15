43adf22b5ffe4ab564160537d48d8e7e4a0b4509
Naohiro MANAGO <manago.naohiro@chiba-u.jp>
2017-09-15 11:06:34 +0900

    NM modified to include edge points.

--- calc_flux.py
+++ calc_flux.py
@@ -10,37 +10,42 @@ try:
 except ImportError:
     from StringIO import StringIO
 
-#NTH = 16
-NTH = 32
+PI = np.pi
+PI2 = 2.0*PI
+PI_2 = 0.5*PI
+NTH = 30
+NPH = 30
 TH_SUN = 20.0 # Solar zenith angle in deg
-PH_SUN = 0.0 # Solar azimuth angle in deg
+#PH_SUN = 210.0 # Solar azimuth angle in deg
+PH_SUN = 110.0 # Solar azimuth angle in deg
 TH_PNL = 0.0 # Zenith angle of solar panel's normal direction in deg
 PH_PNL = 0.0 # Azimuth angle of solar panel's normal direction in deg
 IAER = 1 # Rural aerosol model
 VIS = 20.0 # Visibility in km
 WMIN = 300.0 # Min. wavelength in nm
-WMAX = 1500.0 # Max. wavelength in nm
+#WMAX = 1500.0 # Max. wavelength in nm
+WMAX = 500.0 # Max. wavelength in nm
 MODTRAN_DATDIR = '/usr/local/Mod5.2.0.0/org/DATA'
 MODTRAN_BINDIR = '/usr/local/Mod5.2.0.0/org'
 
-def PolToPol(th,ph,thi,phi,radians=True):
+def Pol0ToPol(th0,ph0,thi,phi,radians=True): # thi,phi ... measured in the (th0,ph0) coordinate
     if radians:
-        th_r = th
-        ph_r = ph
+        th0_r = th0
+        ph0_r = ph0
         thi_r = thi
         phi_r = phi
     else:
-        th_r = np.radians(th)
-        ph_r = np.radians(ph)
+        th0_r = np.radians(th0)
+        ph0_r = np.radians(ph0)
         thi_r = np.radians(thi)
         phi_r = np.radians(phi)
-    th_c = np.cos(th_r)
-    th_s = np.sin(th_r)
-    ph_c = np.cos(ph_r)
-    ph_s = np.sin(ph_r)
-    v_x = np.array([th_c*ph_c,th_c*ph_s,-th_s])
-    v_y = np.array([-ph_s,ph_c,0.0])
-    v_z = np.array([th_s*ph_c,th_s*ph_s,th_c])
+    th0_c = np.cos(th0_r)
+    th0_s = np.sin(th0_r)
+    ph0_c = np.cos(ph0_r)
+    ph0_s = np.sin(ph0_r)
+    v_x = np.array([th0_c*ph0_c,th0_c*ph0_s,-th0_s])
+    v_y = np.array([-ph0_s,ph0_c,0.0])
+    v_z = np.array([th0_s*ph0_c,th0_s*ph0_s,th0_c])
     thi_c = np.cos(thi_r)
     thi_s = np.sin(thi_r)
     phi_c = np.cos(phi_r)
@@ -62,12 +67,12 @@ def run_modtran(th_los=60.0,ph_los=0.0,th_sun=TH_SUN,ph_sun=PH_SUN,iaer=IAER,vis
     with open('modtran.tp5','w') as fp:
         fp.write('mmf+2    3    2    1    0    0    0    0    0    0    0    0    1    .000 0.1000\n')
         fp.write('ft  16 0.0380.0000001.000000001.000000000f t f f\n')
-        fp.write('01_2008\n')
+        fp.write('15_2008\n')
         fp.write('  {:3d}    0    0    0    0    0{:10.5f}.000000000.000000000.000000000.000000000\n'.format(iaer,vis))
         fp.write('    0.0000    0.0000{:10.4f}    0.0000     0.000     0.000    0          0.000\n'.format(th_los))
         fp.write('    2    2   93    0\n')
         fp.write('{:10.4f}{:10.4f}     0.000     0.000     0.000     0.000     0.000     0.000\n'.format(ph_sun-ph_los,th_sun))
-        fp.write('{:10.0f}{:10.0f}       1.0       2.0rw        {:10s}\n'.format(1.0e7/wmax,1.0e7/wmin,' t    f  1' if fluxout else ''))
+        fp.write('{:10.0f}{:10.0f}      15.0      30.0rw        {:10s}\n'.format(1.0e7/wmax,1.0e7/wmin,' t    f  1' if fluxout else ''))
         fp.write('    0\n')
     if not os.path.exists('DATA'):
         os.symlink(MODTRAN_DATDIR,'DATA')
@@ -97,62 +102,59 @@ th_sun_r = np.radians(TH_SUN)
 ph_sun_r = np.radians(PH_SUN)
 th_pnl_r = np.radians(TH_PNL)
 ph_pnl_r = np.radians(PH_PNL)
+th_pnl_c = np.cos(th_pnl_r)
 th_pnl_s = np.sin(th_pnl_r)
-v_pnl = np.array([th_pnl_s*np.cos(ph_pnl_r),th_pnl_s*np.sin(ph_pnl_r),np.cos(th_pnl_r)])
+ph_pnl_c = np.cos(ph_pnl_r)
+ph_pnl_s = np.sin(ph_pnl_r)
+v_pnl = np.array([th_pnl_s*ph_pnl_c,th_pnl_s*ph_pnl_s,th_pnl_c])
 
-dth_r = np.pi/NTH
-dlen = 2.0*np.pi*np.sin(0.5*dth_r)
-k = 0
+dth_r = PI/NTH
+dph_r = PI2/NPH
+tgrd = []
+pgrd = []
+dgrd = []
 for i in range(NTH):
-    th_1 = dth_r*i
-    th_2 = th_1+dth_r
-    th_c = (th_1+th_2)*0.5
-    nph = int(2.0*np.pi*np.sin(th_c)/dlen+0.5)
-    if nph <= 1:
-        nph = 1
-        dph_r = 0.0
-        th_los_r = np.pi if th_c > 0.5*np.pi else 0.0
-    else:
-        dph_r = 2.0*np.pi/nph
-        th_los_r = th_c
-    for j in range(nph):
-        ph_los_r = dph_r*(float(j)+0.5)
-        th_los_o,ph_los_o = PolToPol(th_sun_r,ph_sun_r,th_los_r,ph_los_r,radians=True)
-        if th_los_o > 0.5*np.pi:
+    thi = dth_r*(float(i)+0.5)
+    for j in range(NPH):
+        phi = dph_r*(float(j)+0.5)
+        th_los_r,ph_los_r = Pol0ToPol(th_sun_r,ph_sun_r,thi,phi,radians=True)
+        dp = ph_los_r-ph_sun_r
+        while dp < -PI:
+            dp += PI2
+        while dp > PI:
+            dp -= PI2
+        if th_los_r > PI_2 or dp < 0.0:
             continue
-        k += 1
-kmax = k
-k = 0
-yi = 0.0
-for i in range(NTH):
-    th_1 = dth_r*i
-    th_2 = th_1+dth_r
-    th_c = (th_1+th_2)*0.5
-    nph = int(2.0*np.pi*np.sin(th_c)/dlen+0.5)
-    if nph <= 1:
-        nph = 1
-        dph_r = 0.0
-        th_los_r = np.pi if th_c > 0.5*np.pi else 0.0
-    else:
-        dph_r = 2.0*np.pi/nph
-        th_los_r = th_c
-    domg = 2.0*np.pi*(np.cos(th_1)-np.cos(th_2))/nph
-    for j in range(nph):
-        ph_los_r = dph_r*(float(j)+0.5)
-        th_los_o,ph_los_o = PolToPol(th_sun_r,ph_sun_r,th_los_r,ph_los_r,radians=True)
-        if th_los_o > 0.5*np.pi:
-            continue
-        sys.stderr.write('{:4d} {:4d} {:4d}/{:4d}\n'.format(i,j,k+1,kmax))
-        th_los = np.degrees(th_los_o)
-        ph_los = np.degrees(ph_los_o)
-        sys.stderr.write('{:9.4f} {:9.4f} {:9.4f} {:9.4f}\n'.format(np.degrees(th_los_r),np.degrees(ph_los_r),th_los,ph_los))
-        run_modtran(th_los,ph_los)
-        wi,yr = get_radiance()
-        th_los_s = np.sin(th_los_o)
-        v_los = np.array([th_los_s*np.cos(ph_los_o),th_los_s*np.sin(ph_los_o),np.cos(th_los_o)])
-        yi += yr*np.dot(v_pnl,v_los)*domg
-        k += 1
-run_modtran(0.0,0.0,fluxout=True)
-wf,fu,fd,fs = get_flux()
+        tgrd.append(th_los_r)
+        pgrd.append(ph_los_r)
+        dgrd.append(dp)
+for th_los_r in [0.0,PI_2]:
+    for dp in [0.0,PI_2,PI]:
+        ph_los_r = ph_sun_r+dp
+        while ph_los_r < -PI:
+            ph_los_r += PI2
+        while ph_los_r > PI:
+            ph_los_r -= PI2
+        tgrd.append(th_los_r)
+        pgrd.append(ph_los_r)
+        dgrd.append(dp)
+tgrd = np.array(tgrd)
+pgrd = np.array(pgrd)
+dgrd = np.array(dgrd)
+kmax = tgrd.size
+zgrd = []
+for k in range(kmax):
+    sys.stderr.write('{:4d}/{:4d}\n'.format(k+1,kmax))
+    th_los_r = tgrd[k]
+    ph_los_r = pgrd[k]
+    th_los = np.degrees(th_los_r)
+    ph_los = np.degrees(ph_los_r)
+    run_modtran(th_los,ph_los)
+    wi,yr = get_radiance()
+    zgrd.append(yr)
+zgrd = np.array(zgrd)
+
+#run_modtran(0.0,0.0,fluxout=True)
+#wf,fu,fd,fs = get_flux()
 
 
